a9ada06fbae41a2e977899c6c4d3f3fffd8855d4
kusmawan <30643346+kusmawan2020@users.noreply.github.com>
2025-10-17 23:23:03 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // import dependencies
 //
-import express from 'express';
-import cors from 'cors';
-import multer from 'multer';
-import { GoogleGenAI } from '@google/genai';
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+import { GoogleGenAI } from "@google/genai";
+// SESSION 5 - import path/url package
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-// kita import dan langsung panggil function-nya
-import 'dotenv/config';
+// WIP: kita import dan langsung panggil function-nya
+import "dotenv/config";
 
 // inisialisasi aplikasi
 //
@@ -24,12 +27,28 @@ import 'dotenv/config';
 const app = express();
 const upload = multer(); // akan digunakan di dalam recording
 
-const ai = new GoogleGenAI({ }); // instantiation menjadi object instance (OOP -- Object-Oriented Programming)
+const ai = new GoogleGenAI({}); // instantiation menjadi object instance (OOP -- Object-Oriented Programming)
+
+// SESSION 5 - penambahan path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // inisialisasi middleware
 // contoh: app.use(namaMiddleware());
 app.use(cors()); // inisialisasi CORS (Cross-Origin Resource Sharing) sebagai middleware
 app.use(express.json());
+
+// SESSION 5 - inisialisasi static directory
+app.use(
+  // express.static(rootDirectory, options)
+  express.static(
+    path.join(__dirname, "static"), // rootDirectory
+    // akan set route `/` sebagai static directory, dengan
+    // folder "static" (atau nama yang kita set di atas) sebagai
+    // direktori tujuan; tapi ketika ada route handler
+    // yang di-set di bawahnya, route handler tersebut akan diutamakan
+  ),
+);
 
 // inisialisasi routing
 // contoh: app.get(), app.post(), app.put(), dll
@@ -46,7 +65,7 @@ app.use(express.json());
 // secara alurnya
 // synchronous    --> () => {}
 // [*] asynchronous   --> async () => {}
-app.post('/generate-text', async (req, res) => {
+app.post("/generate-text", async (req, res) => {
   // terima jeroannya, lalu cek di sini
   const { prompt } = req.body; // object destructuring
 
@@ -54,11 +73,11 @@ app.post('/generate-text', async (req, res) => {
   console.log({ prompt });
 
   // guard clause (kasarnya, satpam)
-  if (!prompt || typeof prompt !== 'string') {
+  if (!prompt || typeof prompt !== "string") {
     res.status(400).json({
       success: false,
-      message: 'Prompt harus berupa string!',
-      data: null
+      message: "Prompt harus berupa string!",
+      data: null,
     });
     return;
   }
@@ -66,27 +85,118 @@ app.post('/generate-text', async (req, res) => {
   // jeroannya
   try {
     const aiResponse = await ai.models.generateContent({
-      model: 'gemini-2.5-flash',
-      contents: [
-        { text: prompt }
-      ],
+      model: "gemini-2.5-flash",
+      contents: [{ text: prompt }],
       // ini untuk config AI-nya lebih jauh lagi
       config: {
-        systemInstruction: 'Harus dibalas dalam bahasa Jepang atau Sunda secara acak.'
-      }
+        systemInstruction:
+          "Harus dibalas dalam bahasa Jepang atau Sunda secara acak.",
+      },
     });
 
     res.status(200).json({
       success: true,
-      message: 'Berhasil dijawab sama Gemini nih!',
-      data: aiResponse.text
+      message: "Berhasil dijawab sama Gemini nih!",
+      data: aiResponse.text,
     });
   } catch (e) {
     console.log(e);
     res.status(500).json({
       success: false,
-      message: 'Gagal gan, server-nya kayaknya lagi bermasalah!',
-      data: null
+      message: "Gagal gan, server-nya kayaknya lagi bermasalah!",
+      data: null,
+    });
+  }
+});
+
+// fitur chat
+// endpoint: POST /api/chat
+app.post("/api/chat", async (req, res) => {
+  const { conversation } = req.body;
+
+  try {
+    // satpam #1: Cek conversation apakah berupa array atau tidak
+    //            dengan Array.isArray().
+    if (!Array.isArray(conversation)) {
+      throw new Error("Conversation harus berupa array!");
+    }
+
+    // satpam #2: Cek setiap pesan dalam conversation, apakah valid atau tidak
+    let messageIsValid = true;
+
+    if (conversation.length === 0) {
+      throw new Error("Conversation tidak boleh kosong!");
+    }
+
+    conversation.forEach((message) => {
+      // bisa tambah satu kondisi lagi untuk cek variable messageIsValid
+      // di sini
+
+      // Kondisi #1 -- message harus berupa object dan bukan `null`
+      if (!message || typeof message !== "object") {
+        messageIsValid = false;
+        return;
+      }
+
+      const keys = Object.keys(message);
+      const objectHasValidKeys = keys.every((key) =>
+        ["text", "role"].includes(key),
+      );
+
+      // looping kondisi di dalam array
+      // .every() --> &&-nya si if --> 1 false, semuanya false
+      // .some()  --> ||-nya si if --> 1 true, semuanya menjadi true
+
+      // Kondisi #2 -- message harus punya struktur yang valid
+      if (keys.length !== 2 || !objectHasValidKeys) {
+        messageIsValid = false;
+        return;
+      }
+
+      const { text, role } = message;
+
+      // Kondisi 3A -- role harus valid
+      if (!["model", "user"].includes(role)) {
+        messageIsValid = false;
+        return;
+      }
+
+      // Kondisi 3B -- text harus valid
+      if (!text || typeof text !== "string") {
+        messageIsValid = false;
+        return;
+      }
+    });
+
+    if (!messageIsValid) {
+      throw new Error("Message harus valid!");
+    }
+
+    // proses dagingnya
+
+    const contents = conversation.map(({ role, text }) => ({
+      role,
+      parts: [{ text }],
+    }));
+
+    const aiResponse = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents,
+      config: {
+        systemInstruction: "Harus membalas dengan bahasa Sunda.",
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Berhasil dibalas oleh Google Gemini!",
+      data: aiResponse.text,
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: e.message,
+      data: null,
     });
   }
 });
@@ -95,6 +205,6 @@ app.post('/generate-text', async (req, res) => {
 app.listen(
   3000, // port yang akan diakses
   () => {
-    console.log('I LOVE YOU 3000');
-  }
+    console.log("I LOVE YOU 3000");
+  },
 );
